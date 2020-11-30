5a157b4f2ee3a49067cda97bfb8c62f0f7175d77
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2020-11-30 18:01:33 +0100

    updated the repo

--- creation_tei_corpus.py
+++ creation_tei_corpus.py
@@ -2,14 +2,15 @@ import glob
 import xml.etree.ElementTree as ET
 from lxml import etree
 import sys
+import subprocess
 
 def main(input_dir):
     '''
-    Cette fonction produit un document xml-tei maître permettant de lier chaque division entre elles.
+    This function creates a tei:teiCorpus main file
     :param input_file: le temoin à traiter sans extension pour l'instant
     :return: None
     '''
-    corpus_file = "corpus.xml"
+    corpus_file = "corpus_model.xml"
     xinclude_list = []
     for file in glob.glob(input_dir):
         x_include = f"<xi:include href=\"{file}\" xmlns:xi=\"http://www.w3.org/2001/XInclude\"/>"
@@ -21,10 +22,28 @@ def main(input_dir):
         tei_list = f"<teiCorpus>{' '.join(xinclude_list)}</teiCorpus>"
         body = corpus_file.xpath("//tei:teiCorpus", namespaces=tei)[0]
         body.insert(1, etree.fromstring(tei_list))
-
-
-        with open("corpus_file_out.xml", "w") as outfile:
+        with open("corpus_file.xml", "w") as outfile:
             outfile.write(etree.tostring(corpus_file).decode())
+
+def transformation_xml_1():
+    '''
+    This function modificates the xml:id of the descs and items to fit with Capitains Guidelines
+    :return:
+    '''
+    saxon = 'saxon9he.jar'
+    cmd = f'java -jar {saxon} -xi:on corpus_file.xml data_fitting.xsl'
+    subprocess.run(cmd.split())
+
+
+def transformation_xml_2():
+    '''
+    This fuction creates a folder compliant with Nautilus requirements.
+    :return:
+    '''
+    saxon = 'saxon9he.jar'
+    cmd = f'java -jar {saxon} -xi:on final_corpus_file.xml to_nautilus.xsl'
+    subprocess.run(cmd.split())
+
 
 
 def changement_path(corpus):
@@ -35,13 +54,13 @@ def changement_path(corpus):
         string_corpus = string_corpus.replace("_tagged", "")
         string_corpus = string_corpus.replace("xml/", "output_xml/")
 
-        with open("corpus_file_out2.xml", "w") as outfile:
+        with open("final_corpus_file.xml", "w") as outfile:
             outfile.write(string_corpus)
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "1":
-        files = "xml/*.xml"
-        main(files)
-    else:
-        changement_path("corpus_file_out.xml")
+    files = "xml/*.xml"
+    main(files)
+    transformation_xml_1()
+    changement_path("corpus_file.xml")
+    transformation_xml_2()
